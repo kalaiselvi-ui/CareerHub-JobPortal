49ab2d0fb594846e1e43699b190e6e1d833779c3
Kalai <kalaichandran369@gmail.com>
2026-08-17 07:39:42 +0400

    added vercel domain url in the backend as well as render backend url in frontend

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5000"],
+    origin: [
+      "http://localhost:5173",
+      "https://career-hub-job-portal-tau.vercel.app",
+    ],
     credentials: true,
   }),
 );
